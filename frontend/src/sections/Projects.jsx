ef5ab01a6799bd3c3af4f44e2494a258bc4d3351
Hamza <myselfhamzaaziz@gmail.com>
2024-10-18 22:21:24 +0500

    added marquee in projects

--- frontend/src/sections/Projects.jsx
+++ frontend/src/sections/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SingleProject from '../components/SingleProject'
+import Marquee from "react-fast-marquee";
 
 const Projects = () => {
   return (
@@ -9,25 +10,33 @@ const Projects = () => {
         <span>Key Projects</span>
       </h1>
 
-      <div className='grid grid-cols-1 lg:grid-cols-2 laptop:grid-cols-3 gap-10 mt-16 w-3/4 mx-auto'>
-        <SingleProject
-          title={'Blogs App'}
-          description={'This web application was developed using MERN stack. It provides a platform for users to create, read, update and delete blog posts. Each blog post has a title, summary, image and blog content. For this users have to first go through authentication process which is acheived using JSON Web Tokens or simply JWT. Moreover I also added a rich text editor React Quill so that users can add content in their desired format.'}
-          githubLink={'https://github.com/IamHamzaAziz/blogging-website-mern'}
-          className='border-2 border-red-600 rounded-3xl hover:shadow-lg hover:shadow-red-600 p-8 cursor-pointer flex flex-col justify-between'
-        />
-        <SingleProject
-          title={'Events App'}
-          description={'In this events app, I utilized React and Tailwind CSS for the frontend, and Django along with the Django REST Framework for the backend. The application allows authenticated users to add, update and delete events, while all users can view event listings and details. Additionally, the app features event search functionality and filtering by category for enhanced user experience.'}
-          githubLink={'https://github.com/IamHamzaAziz/events-webapp-django-react'}
-          className='border-2 border-green-600 rounded-3xl hover:shadow-lg hover:shadow-green-600 p-8 space-y-5 cursor-pointer flex flex-col justify-between'
-        />
-        <SingleProject
-          title={'Recipe Sharing App'}
-          description={'This web application enables users to share and save recipes, similar to saving posts on social media platforms. It features user authentication, CRUD operations, and image uploads. Users can also apply filters to find specific recipes. For enhanced security, UUIDs were used instead of regular IDs. The project was built using Django and styled with Tailwind CSS.'}
-          githubLink={'https://github.com/IamHamzaAziz/flavor-quest-django'}
-          className='border-2 border-blue-600 rounded-3xl hover:shadow-lg hover:shadow-blue-600 p-8 space-y-5 cursor-pointer flex flex-col justify-between'
-        />
+      <div className='w-full sm:w-3/4 mx-auto mt-14'>
+        <Marquee pauseOnHover={true} speed={60}>
+          <SingleProject
+            title={'Real Estate Website'}
+            description={'This website is developed using MERN stack, Shadcn UI components and TypeScript in frontend. It has OTP-based user authentication and password reset. The site lists properties with images and details like address, area, and city, allowing users to filter and save listings. Rich text descriptions for properties and blogs were implemented with React Quill. Additionally, the platform includes a contact form and an admin panel for managing properties, blogs, messages, and users.'}
+            githubLink={'https://github.com/IamHamzaAziz/real-estate-website-mern'}
+            className='border-slate-500 hover:shadow-slate-600'
+          />
+          <SingleProject
+            title={'Blogs App'}
+            description={'This web application was developed using MERN stack. It provides a platform for users to create, read, update and delete blog posts. Each blog post has a title, summary, image and blog content. For this users have to first go through authentication process which is acheived using JSON Web Tokens or simply JWT. Moreover I also added a rich text editor React Quill so that users can add content in their desired format.'}
+            githubLink={'https://github.com/IamHamzaAziz/blogging-website-mern'}
+            className='border-red-600 hover:shadow-red-600'
+          />
+          <SingleProject
+            title={'Events App'}
+            description={'In this events app, I utilized React and Tailwind CSS for the frontend, and Django along with the Django REST Framework for the backend. The application allows authenticated users to add, update and delete events, while all users can view event listings and details. Additionally, the app features event search functionality and filtering by category for enhanced user experience.'}
+            githubLink={'https://github.com/IamHamzaAziz/events-webapp-django-react'}
+            className='border-green-600 hover:shadow-green-600'
+          />
+          <SingleProject
+            title={'Recipe Sharing App'}
+            description={'This web application enables users to share and save recipes, similar to saving posts on social media platforms. It features user authentication, CRUD operations, and image uploads. Users can also apply filters to find specific recipes. For enhanced security, UUIDs were used instead of regular IDs. The project was built using Django and styled with Tailwind CSS.'}
+            githubLink={'https://github.com/IamHamzaAziz/flavor-quest-django'}
+            className='border-blue-600 hover:shadow-blue-600'
+          />
+        </Marquee>
       </div>
     </div>
   )
