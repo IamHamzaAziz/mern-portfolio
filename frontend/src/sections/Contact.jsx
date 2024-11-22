ea517735e87424cc0d9dd61aa64f49cfe7303b7c
Hamza <myselfhamzaaziz@gmail.com>
2024-11-22 18:32:01 +0500

    set input fields to empty on success in contact form

--- frontend/src/sections/Contact.jsx
+++ frontend/src/sections/Contact.jsx
@@ -50,6 +50,9 @@ const Contact = () => {
                 if (response.status === 200) {
                     setLoading(false)
                     sucess(response.data)
+                    setName('')
+                    setEmail('')
+                    setMessage('')
                 } else {
                     setLoading(false)
                     failure(response.data)
@@ -103,10 +106,10 @@ const Contact = () => {
                                 color='white'
                                 height={20}
                             />
-                        )  : (
+                        ) : (
                             <>
                                 <span>Send</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-horizontal"><path d="m3 3 3 9-3 9 19-9Z"/><path d="M6 12h16"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-horizontal"><path d="m3 3 3 9-3 9 19-9Z" /><path d="M6 12h16" /></svg>
                             </>
                         )
                     }
