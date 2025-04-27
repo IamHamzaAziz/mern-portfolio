f18594fafc76f791be7489884da6b6f6e1419506
Hamza Aziz <myselfhamzaaziz@gmail.com>
2025-04-27 23:10:56 +0500

    changed text in footer

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='py-10 px-5 text-center'>
-        This portfolio is developed using MERN stack
+      Developed using MERN Stack
     </footer>
   )
 }
